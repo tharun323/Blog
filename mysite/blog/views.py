a07c21d664cac4f44d61f2ce08d2f727a0bcfd22
tharun323 <atkr323@gmail.com>
2018-04-07 12:44:19 +0530

    redirect library is added to fix an error

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
